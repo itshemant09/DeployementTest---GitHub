cbdf1c7a9f07ea47ebfcb42aaf716daebe447d1f
Hemant Bharadwaj <itshemant09@gmail.com>
2018-07-03 13:57:09 +0530

    changes

--- DeployementTest/Controllers/HomeController.cs
+++ DeployementTest/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,6 +13,12 @@ namespace DeployementTest.Controllers
     {
         public ActionResult Index()
         {
+            string accessToken = this.Request.Headers["X-MS-TOKEN-FACEBOOK-ACCESS-TOKEN"];
+            if (accessToken == "")
+            {
+                accessToken = "NULL";
+            }
+            ViewBag.AccessToken = accessToken;
             return View();
         }
 
@@ -24,6 +33,63 @@ namespace DeployementTest.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        private string RequestResponse(string pUrl)
+        {
+            HttpWebRequest webRequest = System.Net.WebRequest.Create(pUrl) as HttpWebRequest;
+            webRequest.Method = "GET";
+            webRequest.ServicePoint.Expect100Continue = false;
+            webRequest.Timeout = 20000;
+
+            Stream responseStream = null;
+            StreamReader responseReader = null;
+            string responseData = "";
+            try
+            {
+                WebResponse webResponse = webRequest.GetResponse();
+                responseStream = webResponse.GetResponseStream();
+                responseReader = new StreamReader(responseStream);
+                responseData = responseReader.ReadToEnd();
+            }
+            catch (Exception exc)
+            {
+                Response.Write("<br /><br />ERROR : " + exc.Message);
+            }
+            finally
+            {
+                if (responseStream != null)
+                {
+                    responseStream.Close();
+                    responseReader.Close();
+                }
+            }
+            return responseData;
+        }
+
+        private string CallFacebookGraphApi(string id, string fields, string accessToken)
+        {
+            string fbGraphiApiRequest = "https://graph.facebook.com/" + id + "?" + fields + "&access_token=" + accessToken;
+            string response = RequestResponse(fbGraphiApiRequest);
+            if (response == "")
+            {
+                response = "<br /><br />Error while accessing the FB Graph API";
+            }
+            string resJson = response.Replace(@"\", string.Empty);
+            var obj = JsonConvert.DeserializeObject(resJson);
+            string formattedJson = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            return formattedJson;
+        }
+        public ActionResult Facebook()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string accessToken = this.Request.Headers["X-MS-TOKEN-FACEBOOK-ACCESS-TOKEN"];
+                // Here we are retrieving the users details
+                string myFields = "fields=first_name,last_name,middle_name,birthday,currency,email";
+                string myJsonResponse = CallFacebookGraphApi("me", myFields, accessToken);
+                ViewBag.Me = myJsonResponse;
+            }
             return View();
         }
     }
---
+++ Deployment-Staging/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Deployment_Staging.MvcApplication" Language="C#" %>
